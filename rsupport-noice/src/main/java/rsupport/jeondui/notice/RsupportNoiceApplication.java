2c42ea195912a9776b8194cf24cfb1cbfdeb9230
jeondui <jeondui@gmail.com>
2024-04-17 18:14:09 +0900

    프로젝트 생성

---
+++ rsupport-noice/src/main/java/rsupport/jeondui/notice/RsupportNoiceApplication.java
@@ -0,0 +1,13 @@
+package rsupport.jeondui.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsupportNoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsupportNoiceApplication.class, args);
+	}
+
+}
---
+++ rsupport-noice/src/test/java/rsupport/jeondui/notice/RsupportNoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rsupport.jeondui.notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsupportNoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
