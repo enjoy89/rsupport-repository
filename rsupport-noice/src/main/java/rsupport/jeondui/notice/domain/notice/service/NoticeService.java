5a9fa24aecb5b43042f45556fbda5a92a77bb580
jeondui <jeondui@gmail.com>
2024-04-18 18:17:54 +0900

    Fix: 첨부파일 입력값이 존재할 경우에만 업로드 하도록 버그 수정

--- rsupport-noice/src/main/java/rsupport/jeondui/notice/domain/notice/service/NoticeService.java
+++ rsupport-noice/src/main/java/rsupport/jeondui/notice/domain/notice/service/NoticeService.java
@@ -1,6 +1,8 @@
 package rsupport.jeondui.notice.domain.notice.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +19,7 @@ import rsupport.jeondui.notice.domain.notice.repository.NoticeRepository;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class NoticeService {
 
     private final NoticeRepository noticeRepository;
@@ -31,13 +34,30 @@ public class NoticeService {
         Member member = memberService.findById(memberId); // 회원 조회
         Notice notice = noticeRepository.save(Notice.of(member, request)); // 공지사항 객체 생성 후 저장
 
-        for (MultipartFile file : request.getFiles()) {
-            // AWS S3 버킷에 첨부파일 업로드
-            String fileName = amazonS3Service.uploadFile(file);
-            String fileType = FileUtil.getExtension(fileName);
-
-            attachmentRepository.save(Attachment.of(notice, fileName, fileType)); // 첨부파일 객체 생성 후 저장
+        // 첨부파일이 존재할 때만 업로드
+        if (isNotEmptyFile(request.getFiles())) {
+            uploadAndSaveAttachments(notice, request.getFiles());
         }
+    }
+
+    /**
+     * 첨부파일 업로드 후 저장
+     */
+    private void uploadAndSaveAttachments(Notice notice, List<MultipartFile> files) {
+        for (MultipartFile file : files) {
+            if (!file.isEmpty()) {
+                String fileName = amazonS3Service.uploadFile(file); // AWS S3 버킷에 첨부파일 업로드
+                String fileType = FileUtil.getExtension(fileName);
+                attachmentRepository.save(Attachment.of(notice, fileName, fileType)); // 첨부파일 객체 생성 후 저장
+            }
+        }
+    }
+
+    /**
+     * 파일 존재 유무 판별
+     */
+    private boolean isNotEmptyFile(List<MultipartFile> files) {
+        return !(files == null || files.isEmpty());
     }
 
     // TODO: 공지사항 수정
