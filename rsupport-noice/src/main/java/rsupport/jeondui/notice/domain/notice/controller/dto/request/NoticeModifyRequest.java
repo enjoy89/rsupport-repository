313c5dd815c562436cbec242062e4bb7e508c442
jeondui <jeondui@gmail.com>
2024-04-20 16:15:16 +0900

    Refactor: 공지사항 수정 요청값 수정

--- rsupport-noice/src/main/java/rsupport/jeondui/notice/domain/notice/controller/dto/request/NoticeModifyRequest.java
+++ rsupport-noice/src/main/java/rsupport/jeondui/notice/domain/notice/controller/dto/request/NoticeModifyRequest.java
@@ -1,7 +1,5 @@
 package rsupport.jeondui.notice.domain.notice.controller.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -15,17 +13,12 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 public class NoticeModifyRequest {
 
-    @NotBlank(message = "제목은 필수 입력값입니다.")
     private String title;
-
-    @NotBlank(message = "내용은 필수 입력값입니다.")
     private String content;
 
-    @NotNull(message = "공지 시작 일시는 필수 입력값입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startDateTime;
 
-    @NotNull(message = "공지 종료 일시는 필수 입력값입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endDateTime;
 
